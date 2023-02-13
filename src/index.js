2dba8612167676754c6435dc655cc4a47b7f8d6d
Anna-R-a <ratnikova_a.m@mail.ru>
2023-02-13 20:32:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,21 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let newExpr = [];
+    newExpr = expr.match(/.{1,10}/g);
+    let morseExpr = [];
+    let result = [];
+    for (let i = 0; i<newExpr.length; i++) {
+        morseExpr.push(newExpr[i].replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-'));
+    }
+    for (let j = 0; j < morseExpr.length; j++) {
+        result.push(MORSE_TABLE[morseExpr[j]]);
+    }
+    return result.join('');
 }
 
 module.exports = {
